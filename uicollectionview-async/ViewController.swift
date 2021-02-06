d718c9bbc6564a51f990e1b15202790dd187f054
cielgrisdemoscou <sunggweon.hyeong@etud.univ-paris8.fr>
2021-02-06 16:02:15 +0900

    initial commit

---
+++ uicollectionview-async/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  uicollectionview-async
+//
+//  Created by Ted on 2021/02/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
